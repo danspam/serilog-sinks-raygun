84f37cf28c6c23f603f605037547ebcb57cd0844
Dan Sargeant <dan@keywordintent.com>
2018-02-10 13:20:58 +1000

    Force use of aspnet core for framework 471

--- src/Serilog.Sinks.Raygun/Sinks/Raygun/RaygunSink.cs
+++ src/Serilog.Sinks.Raygun/Sinks/Raygun/RaygunSink.cs
@@ -1,11 +1,11 @@
 ﻿// Copyright 2014 Serilog Contributors
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -90,7 +90,7 @@ namespace Serilog.Sinks.Raygun
                          .Select(pv => new { Name = pv.Key, Value = RaygunPropertyFormatter.Simplify(pv.Value) })
                          .ToDictionary(a => a.Name, b => b.Value);
 
-            // Add the message 
+            // Add the message
             properties.Add("RenderedLogMessage", logEvent.RenderMessage(_formatProvider));
             properties.Add("LogMessageTemplate", logEvent.MessageTemplate.Text);
 
@@ -138,11 +138,8 @@ namespace Serilog.Sinks.Raygun
             }
 
             // Submit
-#if NETSTANDARD2_0
             _client.Send(raygunMessage).Wait();
-#else
-            _client.Send(raygunMessage);
-#endif
+
         }
     }
 }
